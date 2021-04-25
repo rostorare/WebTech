cb485dd0f63426c6259d958d7fca36e5fd208f45
unknown <mzwyssig93@gmail.com>
2021-04-25 14:13:14 +0200

    Hello World Spring boot

---
+++ WT/WT/src/test/java/htw/berlin/WT/WtApplicationTests.java
@@ -0,0 +1,13 @@
+package htw.berlin.WT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
