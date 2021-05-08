0ea0d08248d1b1da74cf474563c819e16f5346f7
s0568841 <s0568841@htw-berlin.de>
2021-05-09 01:08:52 +0200

    added updated version of system.properties & first tries with the rest controller

---
+++ src/main/java/com/example/springboot/MyService.java
@@ -0,0 +1,11 @@
+package com.example.springboot;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyService {
+    public int computeSomething()
+    {
+        return 42;
+    }
+}
